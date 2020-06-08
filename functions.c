c44fa04fc097e62a08f3f3a91cfc397f2d4cc506
Oscar Olsson <pinnenolsson@hotmail.com>
2020-06-08 18:55:50 +0200

    Display colour based on input

--- functions.c
+++ functions.c
@@ -14,23 +14,21 @@ void renderRandomColours(SDL_Renderer* renderer) {
 	SDL_RenderPresent(renderer);
 	SDL_Delay(1500);
 }
-void renderChosenColour(SDL_Renderer* renderer) {
 
-	int choice = 0;
-	printf("What color do you want to display, input 1 for red, 2 for blue and 3 for green: \n");
-	scanf_s("%d", &choice);
-
-	if (choice == 1) {
-		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	}
-	else if (choice == 2) {
-		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-	}
-	else if (choice == 3) {
-		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-	}
-
+void renderRedColour(SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderClear(renderer);
 	SDL_RenderPresent(renderer);
-	SDL_Delay(1500);
+}
+
+void renderGreenColour(SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+}
+
+void renderBlueColour(SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
 }
--- functions.h
+++ functions.h
@@ -5,8 +5,16 @@
 #include <stdlib.h>
 #include <SDL.h>
 
-void renderRandomColours(SDL_Renderer* renderer);
-void renderChosenColour(SDL_Renderer* renderer);
+enum KeyPresses {
+	KEY_PRESS_UP,
+	KEY_PRESS_DOWN,
+	KEY_PRESS_LEFT,
+	KEY_PRESS_RIGHT
+};
 
+void renderRandomColours(SDL_Renderer* renderer);
+void renderRedColour(SDL_Renderer* renderer);
+void renderGreenColour(SDL_Renderer* renderer);
+void renderBlueColour(SDL_Renderer* renderer);
 
 #endif
